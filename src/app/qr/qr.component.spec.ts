9a42f42b5e3b349fdd73c8b25efb292da46f4c08
Chilo-BGS21 <45158830+Chilo-BGS21@users.noreply.github.com>
2019-06-06 06:54:50 -0600

    CreacionComponenteQR
    Se creo un componente QR para postieriomente agregar codigo

---
+++ src/app/qr/qr.component.spec.ts
@@ -0,0 +1,27 @@
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { QrComponent } from './qr.component';
+
+describe('QrComponent', () => {
+  let component: QrComponent;
+  let fixture: ComponentFixture<QrComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ QrComponent ],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(QrComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
